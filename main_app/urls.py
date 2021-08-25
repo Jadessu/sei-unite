abdf845a0b64d0994e1dac7ea096f0c44a93159a
jadessu <junioradessu@gmail.com>
2021-08-25 13:24:28 -0500

    add project index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name='about'),
-    path('projects', views.projects_index, name='projects_index')
+    path('projects', views.projects_index, name='projects_index'),
+    path('projects/<int:project_id>/', views.project_detail, name='projects_detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -13,3 +13,7 @@ def about(request):
 def projects_index(request):
     projects = Project.objects.all()
     return render(request, 'projects/index.html', {'projects': projects})
+
+def projects_detail(request, project_id):
+    project = Project.objects.get(id=project_id)
+    return render(request, 'projects/detail.html', {'project': project})
