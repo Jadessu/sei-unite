3bc61898afbe3c427134b08858c77049c5cc696c
jadessu <junioradessu@gmail.com>
2021-08-25 13:51:00 -0500

    add detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name='about'),
-    path('projects', views.projects_index, name='projects_index'),
-    path('projects/<int:project_id>/', views.project_detail, name='projects_detail')
+    path('projects/', views.projects_index, name='projects_index'),
+    path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
 ]
