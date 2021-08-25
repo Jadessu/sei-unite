d3401d77bfe1677fd1d8fd55b5c6d29be6975851
jadessu <junioradessu@gmail.com>
2021-08-25 14:34:01 -0500

    add update view for project

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('projects/', views.projects_index, name='projects_index'),
     path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
-    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create')
+    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update')
+
 ]
