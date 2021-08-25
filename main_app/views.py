54ed450397e9c559fbc368c13dd8562f262bbde0
jadessu <junioradessu@gmail.com>
2021-08-25 10:52:51 -0500

    create about view

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hi')
+    return render(request, 'about.html')
 
 def about(request):
     return HttpResponse('about us')
