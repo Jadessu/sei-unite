99bdb57f5c79cf005bf9751125ddf7fe962343db
jadessu <junioradessu@gmail.com>
2021-08-25 15:14:12 -0500

    add almunus model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Alumnus
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Alumnus)
--- main_app/models.py
+++ main_app/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
-    image = models.TextField(max_length=200)
-    lookingfor = models.TextField(max_length=400)
-    repo = models.TextField(max_length=150)
-    completed = models.BooleanField(default=False)
-    alumnus = models.ForeignKey(Alumnus, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.title
-
 class Alumnus(models.Model):
     github = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
@@ -21,3 +9,16 @@ class Alumnus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    image = models.TextField(max_length=200)
+    lookingfor = models.TextField(max_length=400)
+    repo = models.TextField(max_length=150)
+    completed = models.BooleanField(default=False)
+    # alumnus = models.ForeignKey(Alumnus, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
