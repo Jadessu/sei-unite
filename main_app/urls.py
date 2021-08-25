e274206a36ae4140a7a3accf1ab151c5d837fbc6
jadessu <junioradessu@gmail.com>
2021-08-25 15:16:54 -0500

    add alumnus create url

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,11 +3,14 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name='about'),
+    # ---------------------------------PROJECTS---------------------------------------------------------
     path('projects/', views.projects_index, name='projects_index'),
     path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
     path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'),
     path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete'),
+    # ------------------------------------ALUMNI------------------------------------------------------
+    path('alumnus/create/', views.AlumnusCreate.as_view(), name='alumnus_create'),
 
 
 ]
